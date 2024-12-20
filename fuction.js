84c0650d4c61e357fad93dc1fdea6106b9e5df0e
RichardEphraim <makindeayomide2024@gmail.com>
2024-12-20 09:35:09 +0100

    today

--- fuction.js
+++ fuction.js
@@ -1,44 +1,47 @@
-
-
-// Get all "View Image" buttons
+// Select all required elements
 const viewImageButtons = document.querySelectorAll('.view-image-btn');
 const modal = document.querySelector('.image-modal');
-const modalContent = document.querySelector('.modal-content');
-const closeModal = document.querySelector('.close-modal');
-const prevButton = document.querySelector('.modal-nav.prev');
-const nextButton = document.querySelector('.modal-nav.next');
+const modalContent = modal.querySelector('.modal-content');
+const closeModal = modal.querySelector('.close-modal');
+const prevButton = modal.querySelector('.modal-nav.prev');
+const nextButton = modal.querySelector('.modal-nav.next');
+const slideshowItems = document.querySelectorAll('.slideshow-item');
 
 let currentIndex = 0;
 
-// Open modal when the "View Image" button is clicked
+// Open modal when "View Image" button is clicked
 viewImageButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
         currentIndex = index;
-        showImage();
-        modal.style.display = 'flex';
+        openModal();
     });
 });
 
-// Close modal
+// Function to open modal and display the current image
+function openModal() {
+    showImage();
+    modal.style.display = 'flex';
+}
+
+// Function to close modal
 closeModal.addEventListener('click', () => {
     modal.style.display = 'none';
 });
 
-// Show image in the modal based on current index
+// Function to show the current image in the modal
 function showImage() {
+    const currentImage = slideshowItems[currentIndex].querySelector('img').src;
     modalContent.innerHTML = `
-        <img src="${slideshowItems[currentIndex].querySelector('img').src}" alt="Slide ${currentIndex + 1}" />
+        <img src="${currentImage}" alt="Slide ${currentIndex + 1}" />
     `;
 }
 
-// Navigate to previous image
-prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + slideshowItems.length) % slideshowItems.length;
-    showImage();
-});
+// Navigate between images in the modal
+prevButton.addEventListener('click', () => navigateImage(-1));
+nextButton.addEventListener('click', () => navigateImage(1));
 
-// Navigate to next image
-nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % slideshowItems.length;
+// Navigate images based on direction
+function navigateImage(direction) {
+    currentIndex = (currentIndex + direction + slideshowItems.length) % slideshowItems.length;
     showImage();
-});
+}
